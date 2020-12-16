42473c7265e97c433f1cf770c7e6edb22c9318c2
Long Tưởng Thành <18520227@ms.uit.edu.vn>
2020-12-16 09:19:16 +0700

    initial commit

---
+++ QuanLyDuLich2/Helper/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyDuLich2.Helper
+{
+    class Util
+    {
+        public static void ShowMessage(string message,
+        [CallerLineNumber] int lineNumber = 0,
+        [CallerMemberName] string caller = null)
+        {
+            MessageBox.Show(message + " at line " + lineNumber + " (" + caller + ")");
+        }
+        public static void ShowTodoMessage(
+        [CallerLineNumber] int lineNumber = 0,
+        [CallerMemberName] string caller = null)
+        {
+            MessageBox.Show("TODO at line " + lineNumber + " (" + caller + ")");
+        }
+    }
+}
---
+++ QuanLyDuLich2/ViewModel/LoginViewModel.cs
@@ -0,0 +1,73 @@
+﻿using QuanLyDuLich2.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using QuanLyDuLich2.Helper;
+
+namespace QuanLyDuLich2.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        //static public NGUOIDUNG TaiKhoanSuDung; // tao bien static nguoi dung
+
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand LoginCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
+
+        private string _UserName;
+        public string UserName { get => _UserName; set { _UserName = value; OnPropertyChanged(); } }
+        private string _Password;
+        public string Password { get => _Password; set { _Password = value; OnPropertyChanged(); } }
+
+        public LoginViewModel()
+        {
+            //DatabaseCheck.Ins.Check();
+            UserName = "";
+            Password = "";
+
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                if (UserName == null || Password == null)
+                    MessageBox.Show("Mời nhập tài khoản!");
+                Util.ShowMessage("TODO: ");
+                /* TODO:
+                ObservableCollection<NGUOIDUNG> Account = new ObservableCollection<NGUOIDUNG>(DataProvider.Ins.DB.NGUOIDUNGs);
+
+                foreach (var item in Account)
+                {
+                    if (item.TenDangNhap == UserName && item.MatKhau == Password)
+                    {
+                        //Gan static TaiKhoanSuDung
+                        TaiKhoanSuDung = item;
+                        
+                        //MessageBox.Show("Đăng nhập thành công");
+                        p.Close();
+                        return;
+                    }
+
+                }
+                MessageBox.Show("Tài khoản không hợp lệ!");
+              
+                //CheckLogin(p);
+
+                //*/
+            });
+
+            CloseWindowCommand = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) => {
+                p.Close();
+                System.Environment.Exit(1);
+            });
+
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) =>
+            {
+                Password = p.Password;
+            });
+        }
+    }
+}
