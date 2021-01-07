46178b9bd188d7ca9f826d1e79dfa61f6bef2fd7
Long Tưởng Thành <18520227@ms.uit.edu.vn>
2021-01-07 14:52:06 +0700

    new room

---
+++ QuanLyDuLich2/View/Catalog/NewRoom_Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyDuLich2.View.Catalog
+{
+    /// <summary>
+    /// Interaction logic for NewRoom_Page.xaml
+    /// </summary>
+    public partial class NewRoom_Page : Page
+    {
+        public NewRoom_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
