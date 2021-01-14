1ca1d80c494aba9cae969ecee3ae7fdd384cf84e
Thành Trung <18520179@gm.uit.edu.vn>
2021-01-14 09:42:00 +0700

    Newest

--- QuanLyDuLich2/Model/QLDL2_Database.Designer.cs
+++ QuanLyDuLich2/Model/QLDL2_Database.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Document\OOAD\QuanLyDuLich\QuanLyDuLich2\Model\QLDL2_Database.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\UIT2020-2021\QuanLyDuLich\QuanLyDuLich2\Model\QLDL2_Database.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
